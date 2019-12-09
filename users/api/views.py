d39e5d9d1ddceacdeaaaeb8b98f74ba290566cad
moshfiqrony <moshfiqrony@gmail.com>
2019-12-09 12:24:44 +0600

    MR | login logout added to the api

--- users/api/views.py
+++ users/api/views.py
@@ -1,11 +1,34 @@
-from ..models import Users
-from .serializers import UserSerializers
-from rest_framework import viewsets
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import authenticate, logout
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.status import (
+    HTTP_400_BAD_REQUEST,
+    HTTP_404_NOT_FOUND,
+    HTTP_200_OK
+)
 
-class UserView(viewsets.ModelViewSet):
-    queryset = Users.objects.all()
-    serializer_class = UserSerializers
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
+
+@csrf_exempt
+@api_view(["POST"])
+@permission_classes((AllowAny,))
+def login(request):
+    username = request.data.get("username")
+    password = request.data.get("password")
+    if username is None or password is None:
+        return Response({'error': 'Please Provide username and password'}, status=HTTP_400_BAD_REQUEST)
+    user = authenticate(username=username, password=password)
+    if not user:
+        return Response({'error': 'Invalid Credentials'}, status=HTTP_404_NOT_FOUND)
+    token, _ = Token.objects.get_or_create(user=user)
+    return Response({'token': token.key}, status=HTTP_200_OK)
+
+
+@csrf_exempt
+@api_view(['POST'])
+def logoutUser(request):
+    print(request.user)
+    res = request.user.auth_token.delete()
+    return Response({'status': True}, HTTP_200_OK)
