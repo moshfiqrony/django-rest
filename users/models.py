af68210d6bf4d614fac3abc0ddd79a8b58bd6cef
moshfiqrony <moshfiqrony@gmail.com>
2019-12-10 13:03:51 +0600

    MR | UserProfile model created

--- users/models.py
+++ users/models.py
@@ -1,11 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
-class Users(models.Model):
-    title = models.CharField(max_length=100)
-    body = models.TextField()
-    created = models.DateTimeField(auto_now_add=True)
+class UserProfile(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, unique=True)
+    email = models.CharField(max_length=100, null=True, unique=True)
+    is_active = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    is_manager = models.BooleanField(default=False)
+    is_org_admin = models.BooleanField(default=False)
+    is_stuff = models.BooleanField(default=False)
+
+
 
     def __str__(self):
-        return self.title
+        return self.email
