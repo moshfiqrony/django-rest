0666590810ab3ec8be9240da6b3d519e5cdd880d
moshfiqrony <moshfiqrony@gmail.com>
2019-12-10 13:03:33 +0600

    MR | user profile registered

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Users
+from .models import UserProfile
 # Register your models here.
-admin.site.register(Users)
+admin.site.register(UserProfile)
