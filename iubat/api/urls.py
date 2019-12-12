55cd213d87a325190ca49c294c3520e4619ed0fb
moshfiqrony <moshfiqrony@gmail.com>
2019-12-12 13:07:18 +0600

    MR | student ApiView added

--- iubat/api/urls.py
+++ iubat/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-
+from .views import addStudent, updateStudent, Students
 
 urlpatterns = [
-
+    path('addstudent/', addStudent),
+    path('updatestudent/', updateStudent),
+    path('student/', Students.as_view()),
 ]
